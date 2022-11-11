62f4136968f3d560946970e42543c6e643170cc3
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-11-11 12:39:24 +0200

    home page and header directive

--- src/app/directives/hide-header.directive.ts
+++ src/app/directives/hide-header.directive.ts
@@ -1,10 +1,40 @@
-import { Directive } from '@angular/core';
+/* eslint-disable @angular-eslint/use-lifecycle-interface */
+import { Directive, HostListener, Input, Renderer2 } from '@angular/core';
+import { DomController, isPlatform } from '@ionic/angular';
 
 @Directive({
-  selector: '[appHideHeader]'
+  selector: '[appHideHeader]',
 })
 export class HideHeaderDirective {
+  @Input('appHideHeader') header: any;
 
-  constructor() { }
+  private headerHeight = isPlatform('ios') ? 44 : 56;
+  private children: any;
 
+  constructor(private renderer: Renderer2, private domCtrl: DomController) {}
+
+  ngAfterViewInit(): void {
+    this.header = this.header.el;
+    this.children = this.header.children;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @HostListener('ionScroll', ['$event']) onContentScroll($event: any): void {
+    const scrollTop: number = $event.detail.scrollTop;
+    let newPosition = -scrollTop;
+
+    if (newPosition < -this.headerHeight) {
+      newPosition = -this.headerHeight;
+    }
+
+    const newOpacity = 1 - newPosition / -this.headerHeight;
+
+    this.domCtrl.write(() => {
+      this.renderer.setStyle(this.header, 'top', newPosition + 'px');
+
+      for (const c of this.children) {
+        this.renderer.setStyle(c, 'opacity', newOpacity);
+      }
+    });
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @angular-eslint/use-lifecycle-interface */
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  categories = [];
+  highlights = [];
+  featured = [];
 
-  constructor() {}
+  catSlideOpts = {
+    freeMode: true,
+    slidesPerView: 3.5,
+    slidesOffsetBefore: 11,
+    spaceBetween: 10,
+  };
 
+  highlightSlideOpts = {
+    slidesPerView: 1.05,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+  };
+
+  featuredSlideOpts = {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    freeMode: true,
+  };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json')
+      .subscribe({
+        next: (response: any) => {
+          this.categories = response.categories;
+          this.highlights = response.highlights;
+          this.featured = response.featured;
+        },
+      });
+  }
+
+  onRefresh(event): void {
+    setTimeout(() => {
+      event.target.complete();
+    }, 2000);
+  }
 }
